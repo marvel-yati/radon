c80e9e5b53346d8ba3bf655714f8616e3348a304
Debayati Sarkar <debayatisarkar@gmail.com>
2022-06-08 02:03:28 +0530

    mongoDB Assignment session 3

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,12 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
+const AuthorModel = require("../models/authorModel")
+
+const createAuthor = async function(req, res){
+    let data = req.body
+    let savedData = await AuthorModel.create(data)
+    res.send({msg: savedData})
+}
 
 const createBook= async function (req, res) {
     let data= req.body
@@ -8,43 +15,35 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+const getBooksByChetanBhagat = async function(req, res) {
+    let data = await AuthorModel.find({author_name:"Chetan Bhagat"}).select("author_id")
+    let bookData = await BookModel.find({author_id:data[0].author_id})
+    res.send({msg:bookData})
 }
 
-
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
+const authorOfBook = async function(req, res) {
+    let data = await BookModel.findOneAndUpdate({name:"Two states"}, {$set:{price:100}},{new:true})
+    let authorData = await AuthorModel.find({author_id:data.author_id}).select("author_name")
+    let prices = data.price
+    res.send({msg:authorData, prices})
 }
 
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
+const bookBetween50_100 = async function(req, res) {
+    let bookBetween = await BookModel.find( { price : { $gte: 50, $lte: 100} } ).select( {author_id: 1 })
+    let author = await AuthorModel.find()
+    let authors=[]
+    bookBetween.map(item=>{
+        author.map(item1=>{
+            if(item.author_id==item1.author_id){
+                authors.push(item1.author_name)
+                //authors.push(item1.name)
+            }
+        })
+    }
+
+    )
+    res.send({msg:authors})
 }
-
-
-
-
 // CRUD OPERATIONS:
 // CREATE
 // READ
@@ -53,7 +52,11 @@ const deleteBooks= async function (req, res) {
 
 
 
+module.exports.createAuthor = createAuthor
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
+module.exports.getBooksByChetanBhagat = getBooksByChetanBhagat
+module.exports.authorOfBook = authorOfBook
+module.exports.bookBetween50_100 = bookBetween50_100
+// module.exports.getBooksData= getBooksData
+// module.exports.updateBooks= updateBooks
+// module.exports.deleteBooks= deleteBooks
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mongo:ofxqtb2WRbhEvU27@cluster0.hqgzvu1.mongodb.net/debayatidb", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 6000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 6000))
 });
