fa1de794e3e5eb7ec3c3da7042abb7ea8da6e78f
Debayati Sarkar <debayatisarkar@gmail.com>
2022-06-10 22:12:00 +0530

    reference-populate

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,42 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
+    if(!req.body.author) return res.send("Author Id is required")
+    if(!req.body.publisher) return res.send("Publisher is required")
+    let authorId = await authorModel.findById(req.body.author)
+    if(!authorId) return res.send("Author is not present")
+    let publish = await publisherModel.findById(req.body.publisher)
+    if(!publish) return res.send("Publisher is not present")
+
     let book = req.body
     let bookCreated = await bookModel.create(book)
     res.send({data: bookCreated})
+    
 }
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
+// const getBooksData= async function (req, res) {
+//     let books = await bookModel.find()
+//     res.send({data: books})
+// }
+
+// const getBooksWithAuthorDetails = async function (req, res) {
+//     let specificBook = await bookModel.find().populate('_id')
+//     res.send({data: specificBook})
+
+// }
+
+const getBooksWithAuthorDetailsAndPublisherDetails = async function (req, res) {
+    let specificBookPublisher = await bookModel.find().populate(['author','publisher'])
+    //console.log(specificBookPublisher)
+    res.send({data: specificBookPublisher})
+
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
 
-}
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+// module.exports.getBooksData= getBooksData
+// module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.getBooksWithAuthorDetailsAndPublisherDetails = getBooksWithAuthorDetailsAndPublisherDetails
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,16 +2,29 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
+    
+    // _id: {
+    //     type: ObjectId,
+    //     ref: "NewPublishers",
+    //     ref: "NewAuthors"
+    // },
     name: String,
-    author_id: {
-        type: ObjectId,
-        ref: "Author"
+    author: {
+        type:ObjectId,
+        ref:"NewAuthors"
     },
     price: Number,
-    ratings: Number
-
+    ratings: Number,
+    publisher: {
+        type: ObjectId,
+        ref: "NewPublishers"
+    },
+    isHardCover:{
+        type: Boolean,
+        default: false
+    }
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('NewBooks', bookSchema)//newbooks
