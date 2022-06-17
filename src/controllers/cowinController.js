6d049d4b61731acfb9e51ea05fdd81b646fa532b
Debayati Sarkar <debayatisarkar@gmail.com>
2022-06-18 03:58:43 +0530

    axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -78,8 +78,75 @@ let getOtp = async function (req, res) {
     }
 }
 
+let getVaccinationFromDistrict = async function (req, res) {
+    try {
+        let districtid = req.query.districtId
+        let date = req.query.date
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtid}&date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+let getTemperature = async function (req, res) {
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        //let districtid = req.query.districtId
+        //let date = req.query.date
+        let cityTemps = []
+        for(let city of cities){
+            let options = {
+                method: "get",
+                url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d528a6d1c0fd73798ba3310a081a780f`
+            }
+        let result = await axios(options);
+        cityTemps.push({City: city, Temperature : result.data.main.temp})
+        }
+        cityTemps = cityTemps.sort(function(a,b){return a.Temperature - b.Temperature})
+        
+        res.status(200).send({ msg: cityTemps, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+let getMeme = async function (req, res) {
+    try {
+        let templateid = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let username = req.query.username
+        let password = req.query.password
+        let options = {
+            method: "get",
+            url: `https://api.imgflip.com/caption_image?template_id=${templateid}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getVaccinationFromDistrict = getVaccinationFromDistrict
+module.exports.getTemperature = getTemperature
+module.exports.getMeme = getMeme
